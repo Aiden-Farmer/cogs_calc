30d64ba11bf08fe20bb9cf9dc124b2f377d32ba8
Aiden-Farmer <aidenfarmer129@gmail.com>
2026-07-07 15:36:19 -0700

    need to figure out RowLike/InventoryRow hinting

--- main.py
+++ main.py
@@ -1,70 +1,103 @@
 from datetime import datetime as dt
 from openpyxl import load_workbook
-from typing import Generator
+from typing import Iterator
+from abc import ABC, abstractmethod
 
-def main():
-    cost_header = {
-        'sku': 6,
-        'qty': 8,
-        'unit_cost': 12,
-        'date': 7,
-        'date_format': '%m-%d-%Y'
-    }
+class RowLike(ABC):
+    def __init__(self, row: list[str]):
+        self.sku = row[0]
 
-    inventory_header = {
-        'sku': 3,
-        'inventory': 29
-    }
+    @classmethod
+    @abstractmethod
+    def from_row(cls, row) -> RowLike|None:
+        row_like = cls(row)
+        return row_like
     
 
-
-class reader:
-    def __init__(self, wb_path, ws_name):
+class Reader:
+    def __init__(self, wb_path, ws_name, return_type: type[RowLike]):
         wb = load_workbook(filename = wb_path)
         self.ws = wb[ws_name]
+        self.rt = return_type
+
+    def readlineo(self) -> Iterator[RowLike]:
+        for raw in self.ws.iter_rows(values_only=True):
+            if (row := self.rt.from_row(raw)):
+                yield row 
 
 
-        
-        
+class LandedCostRow(RowLike):
+    def __init__(self, row: list[str]):
 
-    def readlineo(self) -> Generator[list[str]]:
-        return self.ws.next
+        header = {
+            'sku': 6,
+            'qty': 8,
+            'unit_cost': 12,
+            'date': 7,
+            'date_format': '%m-%d-%Y'
+        }
 
-
-
-
-
-
-class LandedCostRow:
-    def __init__(self, header: dict[str, int], row: list[str]):
         self.sku = row[header['sku']]
         self.qty = row[header['qty']]
         self.unit_cost = row[header['unit_cost']]
-        self.date = dt.strftime(row[header['date']], header['date_format'])
+        self.date = dt.strptime(row[header['date']], header['date_format']) if not isinstance(header['date'], dt) else header['date']
+
+    @classmethod
+    def from_row(cls, row) -> RowLike|None:
+        return super().from_row(row)
 
 
-class InventoryRow:
-    def __init__(self, header: dict[str, int], row: list[str]):
+class InventoryRow(RowLike):
+    def __init__(self, row: list[str]):
+        header = {
+            'sku': 3,
+            'base_sku': 2,
+            'inventory': 29
+        }
+        
         self.sku = row[header['sku']]
+        self.base_sku = row[header['base_sku']]
         self.inventory = row[header['inventory']]
         self.unallocated = self.inventory
         self.purchase_dates = []
         self.excluded_dates = []
+        self.total_cost = 0
         self.average_cost = None
+
+    @classmethod
+    def from_row(cls, row) -> RowLike|None:
+        inv_row = cls(row)
+        if inv_row.sku != inv_row.base_sku:
+            return None
+        return inv_row
 
     def allocate_from_landed_cost(self, cost_row: LandedCostRow):
         if self.inventory == 0:
             self.excluded_dates.append(cost_row.date)
             return
     
-        elif cost_row.qty > self.unallocated:
-            self.unit_cost += self.unallocated * cost_row.unit_cost
+        elif cost_row.qty >= self.unallocated:
+            self.total_cost += self.unallocated * cost_row.unit_cost
             self.purchase_dates.append(cost_row.date)
 
         else:
-            self.unit_cost += self.allocated * cost_row.unit_cost
+            self.total_cost += (cost_row.qty * cost_row.unit_cost)
+            self.unallocated -= cost_row.qty
             self.purchase_dates.append(cost_row.date)
 
 
+
+def main():
+    inv_reader = Reader('inventory.xlsx', 'Inventory', InventoryRow)
+    cost_reader = Reader('landed_cost.xlsx', 'PURCHASES', LandedCostRow)
+
+    inventory: dict[str, RowLike] = {}
+
+    for inv_row in inv_reader.readlineo():
+        inventory[inv_row.sku] = inv_row
+
+    for cost_row in cost_reader.readlineo():
+        if cost_row.sku in inventory:
+            inventory[cost_row.sku].allocate_from_landed_cost(cost_row)
 
     
