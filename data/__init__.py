369bbe9892a0fd81c537964958f395b496802422
Aiden-Farmer <aidenfarmer129@gmail.com>
2026-08-04 09:45:31 -0700

    add FailedRow and include both FailedRow and RowLike in * import

--- data/__init__.py
+++ data/__init__.py
@@ -2,13 +2,14 @@ from __future__ import annotations
 
 from . import excel
 from .data_exception import DataSourceError
-from .datarows import Header, InventoryRow, LandedCostRow, FailedRow
+from .datarows import FailedRow, Header, InventoryRow, LandedCostRow, RowLike
 
 __all__ = [
+    "DataSourceError",
+    "FailedRow",
     "Header",
     "InventoryRow",
     "LandedCostRow",
-    "FailedRow",
+    "RowLike",
     "excel",
-    "DataSourceError",
 ]
